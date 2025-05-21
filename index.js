8ef98c689b071cde17143acc08c52e05c525f0e5
NandaPayy <npayyappilly@ucsd.edu>
2025-05-20 19:10:54 -0700

    updated with margin

--- index.js
+++ index.js
@@ -11,6 +11,32 @@ const profileStats = document.querySelector('#profile-stats');
 
 if (profileStats) {
   profileStats.innerHTML = `
+    <h2 class="stats-title">GitHub Stats</h2>
+    <dl class="stats">
+      <div class="stat-block">
+        <dt>Public Repos:</dt>
+        <dd>${githubData.public_repos}</dd>
+      </div>
+      <div class="stat-block">
+        <dt>Public Gists:</dt>
+        <dd>${githubData.public_gists}</dd>
+      </div>
+      <div class="stat-block">
+        <dt>Followers:</dt>
+        <dd>${githubData.followers}</dd>
+      </div>
+      <div class="stat-block">
+        <dt>Following:</dt>
+        <dd>${githubData.following}</dd>
+      </div>
+    </dl>
+  `;
+}
+
+/* const profileStats = document.querySelector('#profile-stats');
+
+if (profileStats) {
+  profileStats.innerHTML = `
     <h2>GitHub Stats</h2>
     <dl>
       <dt>Public Repos:</dt><dd>${githubData.public_repos}</dd>
@@ -19,4 +45,4 @@ if (profileStats) {
       <dt>Following:</dt><dd>${githubData.following}</dd>
     </dl>
   `;
-}
+} */
